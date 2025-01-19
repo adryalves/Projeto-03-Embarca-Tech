cfceb69bdd0ad8d21a19061929a0e78a8c2f5a2d
adryalves <adryellenalves09@gmail.com>
2025-01-19 13:15:15 -0300

    criei conexões GPIO para os leds e o buzzer

--- Projeto3.c
+++ Projeto3.c
@@ -1,6 +1,26 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
 
+#define LED_GREEN 11
+#define LED_BLUE 12
+#define LED_RED 13
+#define BUZZER_PIN 21
+
 int main()
 {
+    gpio_init(LED_GREEN);
+    gpio_set_dir(LED_GREEN, GPIO_OUT);
+    gpio_put(LED_GREEN, 0);
+
+    gpio_init(LED_BLUE);
+    gpio_set_dir(LED_BLUE, GPIO_OUT);
+    gpio_put(LED_BLUE, 0);
+
+    gpio_init(LED_RED);
+    gpio_set_dir(LED_RED, GPIO_OUT);
+    gpio_put(LED_RED, 0);
+
+    gpio_init(BUZZER_PIN);
+    gpio_set_dir(BUZZER_PIN, GPIO_OUT);
+    gpio_put(BUZZER_PIN, 0);
 }
